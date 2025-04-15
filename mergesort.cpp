35283d02652dc1adcb16d4ca91168ea3349f14a6
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-04-15 10:12:58 +0600

    create sort func

--- mergesort.cpp
+++ mergesort.cpp
@@ -12,3 +12,12 @@ void input() {
         if (n <= 20) break;
         else cout << "Please enter a number less than or equal to 20." << endl;
     }
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
+// Create function to merge two sorted arrays
+void mergesort(int low, int high) {
